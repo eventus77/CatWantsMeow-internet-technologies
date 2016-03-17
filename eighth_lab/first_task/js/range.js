a3a117343281ac1f541b075e2803c4ab9fb1a5de
CatWantsMeow <cat.wants.meow@gmail.com>
2016-03-17 23:14:58 +0300

    Completed first task of eighth lab

---
+++ eighth_lab/first_task/js/range.js
@@ -0,0 +1,36 @@
+function parseInt(value) {
+    var intValue = +value;
+    if (isNaN(intValue)) {
+        throw new Error(value + " is not a number");
+    }
+    return intValue;
+}
+
+
+function range(start, stop, step) {
+    values = [];
+    if (start < stop) {
+        for (var value = start; value <= stop; value += step) {
+            values.push(value);
+        }
+    }
+    else {
+        for (var value = start; value >= stop; value += step) {
+            values.push(value);
+        }
+    }
+    return values;
+}
+
+
+try {
+    start = parseInt(prompt("Введите начало последовательности:"));
+    stop = parseInt(prompt("Введите конец последовательности:"));
+    step = parseInt(prompt("Введите шаг последовательности:"));
+    if (step == 0) throw new Error("Шаг не может быть равен 0");
+
+    alert(range(start, stop, step));
+}
+catch (e) {
+    alert(e.message);
+}
